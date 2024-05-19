b9fb5cbb4103d3f6a45add503c78625e34c35001
serhat-coskun <serhatcoskun187424@gmail.com>
2024-05-20 00:05:31 +0300

    problematic requirements file deleted

--- main2.py
+++ main2.py
@@ -1,7 +1,6 @@
 from codebase.parser.parse import *
 from codebase.util.utils import *
-from codebase.revenue_excel.clean_excel import * 
-from matplotlib import pyplot as plt
+from codebase.revenue_excel.clean_excel import * ß
 import threading
 
 dict_us = parse_loop_us(file_path='./persistance/data/a.csv')
