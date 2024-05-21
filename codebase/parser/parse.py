b4335bdaae46fd37c797955ddabecaf0e323fdb0
ardagulersoy <agulersoy19@ku.edu.tr>
2024-05-21 22:26:23 +0300

    helium parser updated

--- codebase/parser/parse.py
+++ codebase/parser/parse.py
@@ -92,6 +92,8 @@ def parse_loop_us(file_path):
     options.add_argument('--disable-gpu')  # Disable GPU hardware acceleration
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'+ 
+    #                     'KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36')
     options.add_extension('extensions/helium10_extension.crx')
     # options.add_argument(f'--display={display}')  # Use the virtual display
     service = Service(ChromeDriverManager().install())
